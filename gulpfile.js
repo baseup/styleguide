4c3bb4175a80deaaaac6acd0105472db4aad7f54
Mark Serbol <markserbol@gmail.com>
2018-06-20 17:39:34 +0800

    page generator

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var yargs       = require('yargs');
+var path        = require('path');
+var rename      = require('gulp-rename');
+var template    = require('gulp-template');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -11,7 +15,7 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch(["stylesheets/*.scss"], ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch(["*.html", "js/*.js", "templates/*.mst"]).on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -20,6 +24,23 @@ gulp.task('sass', function() {
         .pipe(sass())
         .pipe(gulp.dest("css/"))
         .pipe(browserSync.stream());
+});
+
+gulp.task('page', function() {
+  var cap = function(val) {
+    return val.charAt(0).toUpperCase() + val.slice(1);
+  };
+  var name = yargs.argv.name;
+
+  return gulp.src(path.join(__dirname, 'generator', 'page/**/*.**'))
+    .pipe(template({
+      name: name,
+      upCaseName: cap(name)
+    }))
+    .pipe(rename(function(path) {
+      path.basename = path.basename.replace('temp', name);
+    }))
+    .pipe(gulp.dest(__dirname));
 });
 
 gulp.task('default', ['serve']);
